4bdadb7d19438844bdd52e159b7d56b8816db02e
Michael Chan <michchandev@gmail.com>
2021-03-17 22:34:01 +0800

    feat: setup typescript; move head to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
