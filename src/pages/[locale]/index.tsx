6eed3fd1f7ccb7315eddf3135a939450a6593fef
Michael Chan <michchandev@gmail.com>
2021-03-22 23:18:07 +0800

    feat: Add getStaticPaths and getStaticProps for [locale]/index

--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -1,6 +1,9 @@
 import { FunctionComponent } from 'react'
 import { useRouter } from 'next/router'
-import { getFallbackLocale } from 'utils/i18n'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+import { getFallbackLocale, getLocalesPaths } from 'utils/i18n'
 import { companyList } from 'constants/companies'
 
 const Home: FunctionComponent = () => {
@@ -15,5 +18,21 @@ const Home: FunctionComponent = () => {
 }
 
 Home.displayName = 'Home'
+
+// @REASON: required by NextJS
+// eslint-disable-next-line require-await
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: getLocalesPaths(),
+  fallback: false,
+})
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const locale = (params?.locale || getFallbackLocale()) as string
+  return {
+    props: {
+      ...await serverSideTranslations(locale, ['common']),
+    },
+  }
+}
 
 export default Home
