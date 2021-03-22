9f1931af9d5c637260b7783bbb51a521da1df36c
Michael L.S. Chan <michael.ls.chan@axa.com.hk>
2021-03-22 17:23:01 +0800

    Remove api

--- src/pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextApiRequest, NextApiResponse } from 'next'
-import { StatusCodes } from 'http-status-codes'
-
-export default (req: NextApiRequest, res: NextApiResponse): void => {
-  res.status(StatusCodes.OK).json({ name: 'John Doe' })
-}
