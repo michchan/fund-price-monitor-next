e4274ada34980b9ecc3a3eb8f37607da139180fb
Michael Chan <michchandev@gmail.com>
2021-04-10 23:02:59 +0800

    feat: render company records data in raw elements

--- src/components/pages/CompanyHome/CompanyHome.tsx
+++ src/components/pages/CompanyHome/CompanyHome.tsx
@@ -1,18 +1,71 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useCallback, FocusEvent, ChangeEvent } from 'react'
 import { useTranslation } from 'next-i18next'
-import { CompanyType, FundPriceRecord } from '@michchan/fund-price-monitor-lib'
+import { CompanyType, FundPriceRecordWithDetails, Languages } from '@michchan/fund-price-monitor-lib'
 
 import styles from './CompanyHome.module.scss'
 import PageDocumentHead from 'components/molecules/PageDocumentHead'
 import PageFooter from 'components/molecules/PageFooter'
+import { companyList } from 'constants/companies'
+import { useRouter } from 'next/router'
 
 export interface Props {
   company: CompanyType;
-  records: FundPriceRecord<'mpf', 'latest'>[];
+  records: FundPriceRecordWithDetails<'mpf', 'latest'>[];
 }
 
-const CompanyHome: FunctionComponent<Props> = ({ company }) => {
-  const { t } = useTranslation('common')
+const CompanyHome: FunctionComponent<Props> = ({ company, records }) => {
+  const router = useRouter()
+  const { t, i18n } = useTranslation('common')
+
+  const handleCompanySelectChange = useCallback((
+    e: FocusEvent<HTMLSelectElement> | ChangeEvent<HTMLSelectElement>
+  ) => {
+    const { value } = e.target
+    router.push(`/${i18n.language}/${value}`, undefined, { locale: i18n.language })
+  }, [i18n.language, router])
+
+  const renderCompanySelect = () => (
+    <select
+      onBlur={handleCompanySelectChange}
+      onChange={handleCompanySelectChange}
+      value={company}>
+      {companyList.map(com => (
+        <option
+          key={com}
+          label={com}
+          value={com}/>
+      ))}
+    </select>
+  )
+
+  const renderRecordsTable = () => (
+    <table>
+      <thead>
+        <tr>
+          <th>{'Code'}</th>
+          <th>{'Name'}</th>
+          <th>{'Price'}</th>
+          <th>{'Day +/-'}</th>
+          <th>{'Risk Level'}</th>
+          <th>{'Updated Date'}</th>
+          <th>{'Recorded Time'}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {records.map(r => (
+          <tr key={`${company}-${r.code}`}>
+            <td>{r.code}</td>
+            <td>{r.name[i18n.language as Languages] || r.name.en}</td>
+            <td>{r.price}</td>
+            <td>{`${Number(r.dayPriceChangeRate)?.toFixed(2)}%`}</td>
+            <td>{r.riskLevel}</td>
+            <td>{r.updatedDate}</td>
+            <td>{r.time}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 
   return (
     <div className={styles.container}>
@@ -23,12 +76,10 @@ const CompanyHome: FunctionComponent<Props> = ({ company }) => {
           <p className={styles.titleSection_subtitle}>{t('subtitle')}</p>
         </section>
         <section className={styles.filterSection}>
-          <input
-            type='select'
-            value={company}/>
+          {renderCompanySelect()}
         </section>
         <section className={styles.tableSection}>
-          <div>{'table'}</div>
+          {renderRecordsTable()}
         </section>
       </main>
       <PageFooter/>
