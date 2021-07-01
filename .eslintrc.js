06ac7ffd0be01b9312d5200a17dd1bfadeaf47f9
Michael Chan <michchandev@gmail.com>
2021-07-01 23:31:41 +0800

    chore: upgrade "@michchan/eslint-config": "1.9.0"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,3 @@
 module.exports = {
   extends: '@michchan/eslint-config/react-ts',
-  rules: {
-    'prefer-destructuring': 'warn',
-    'react/no-array-index-key': 'warn',
-    'react/jsx-no-bind': 'warn',
-    '@typescript-eslint/no-magic-numbers': 'warn',
-  },
 }
